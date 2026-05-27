a467133dd32b2ed9220d533147ce830ad01ade36
rajarajanrajagopalan1997 <sathya444000@gmail.com>
2026-05-27 21:19:34 +0530

    change api url

--- src/app/tiny-url/tiny-url.service.ts
+++ src/app/tiny-url/tiny-url.service.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 })
   export class TinyUrlService{
     
-     apiUrl = 'https://localhost:7078/api/';
+     apiUrl = 'https://tiny-url-api.azurewebsites.net/';
 
   constructor(private http: HttpClient) { }
 
